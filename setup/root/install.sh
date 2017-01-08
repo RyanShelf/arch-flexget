de5be54c703ce9bbf4a85c1a1de789357c24b159
Ryan Fraser <ryan@redshelf.com>
2017-01-08 16:27:25 -0600

    add deluge plugin

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -3,14 +3,18 @@
 # exit script if return code != 0
 set -e
 
+
 # define pacman packages
-pacman_packages="python2 python2-pip"
+pacman_packages="python2 python2-pip unrar deluge"
 
 # install pre-reqs
 pacman -Sy --needed $pacman_packages --noconfirm
 pip2 install --upgrade setuptools
 pip2 install flexget
 pip2 install transmissionrpc
+pip2 install service_identity
+pip2 install deluge
+pip2 install rarfile
 
 # call aor script (arch official repo)
 # source /root/aur.sh
